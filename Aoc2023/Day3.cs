640c8f533459a3552aae867063d108b1a8e380d3
Filip Šišmiš <filips@nextitsk.onmicrosoft.com>
2023-12-12 18:06:32 +0100

    starting solving for day 3

---
+++ Aoc2023/Day3.cs
@@ -0,0 +1,77 @@
+﻿namespace AdventOfCode
+{
+    public class Day3
+    {
+        
+        public int SumOfEngineParts()
+        {
+            int sumOfEnignes = 0;
+            var charArray = LoadDataIntoArrays();
+            int nrOfRows = charArray.GetLength(0);
+            int nrOfCols = charArray.GetLength(1);
+
+            for (int y = 0; y < nrOfRows; y++)
+            {
+                for (int x = 0; x < nrOfCols; x++)
+                {
+                    if (IsSymbol(charArray[y, x]))
+                    {
+                        var digitIndexesAround = GetDigitIndexesAroundASymbol(charArray, y, x);
+                        if (digitIndexesAround.Any())
+                        {
+                            foreach(var index in digitIndexesAround)
+                            {
+                                sumOfEnignes += GetEnginePartsAroundSymbolSummed(charArray, index.Item1, index.Item2);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return sumOfEnignes;
+        }
+
+        private char[,] LoadDataIntoArrays()
+        {
+            string[] lines = File.ReadAllLines("d3in.txt");
+
+            int rows = lines.Length;
+            int cols = lines[0].Length;
+
+            char[,] charArray = new char[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                char[] lineChars = lines[i].ToCharArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    charArray[i, j] = lineChars[j];
+                }
+            }
+            return charArray;
+        }
+
+        private bool IsSymbol(char c) => !char.IsDigit(c) && !c.Equals('.');
+
+        private List<(int, int)> GetDigitIndexesAroundASymbol(char[,] charArray, int y, int x)
+        {
+            //would add check for index out of range when checking around a symbol but my input doesn't have any symbols on the border
+            List<(int, int)> tupleList = new();
+            for(int checkY = -1; checkY < 2; checkY++)
+            {
+                for(int checkX = -1; checkX < 2; checkX++)
+                {
+                    if (char.IsDigit(charArray[y + checkY, x + checkX]))
+                        tupleList.Add((y + checkY, x + checkX));
+                }
+            }
+            return tupleList;
+        }
+
+        private int GetEnginePartsAroundSymbolSummed(char[,] charArray, int y, int x ) 
+        {
+
+            return -1;
+        }
+    }
+}
--- Aoc2023/Program.cs
+++ Aoc2023/Program.cs
@@ -12,12 +12,15 @@ public class Program
         //var day1Part2Result = day1.SumAllCalibrationValuesPartTwo();
         //Console.WriteLine($"Day 1 - sum of all calibration values part two is:{day1Part2Result}");
 
-        var day2 = new Day2();
+        //var day2 = new Day2();
 
         //var day2Part1Result = day2.SumOfPossibleGameIds();
         //Console.WriteLine($"Day 2 - sum of all possible game ids is:{day2Part1Result}");
 
-        var day2Part2Result = day2.SumOfPowerForAllTheGameSets();
-        Console.WriteLine($"Day 2 - sum of power of sets from all games is:{day2Part2Result}");
+        //var day2Part2Result = day2.SumOfPowerForAllTheGameSets();
+        //Console.WriteLine($"Day 2 - sum of power of sets from all games is:{day2Part2Result}");
+
+        var day3 = new Day3();
+        day3.SumOfEngineParts();
     }
 }
