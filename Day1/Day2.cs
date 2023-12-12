ac1903c794b2286ad045db1ad06c83546b283ad5
Filip Šišmiš <filips@nextitsk.onmicrosoft.com>
2023-12-12 12:27:40 +0100

    start of day 2

---
+++ Day1/Day2.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode
+{
+    public class Day2
+    {
+        const int maxNrOfRed = 12;
+        const int maxNrOfGreen = 13;
+        const int maxNrOfBlue = 14;
+
+        public int SumOfPossibleGameIds()
+        {
+            var sumOfAll = 0;
+            var lines = File.ReadLines("d2input.txt");
+
+            foreach (var line in lines)
+            {
+                var dataSplit = line.Split(':');
+                var gameid = int.Parse(dataSplit[0].Split(' ')[1].Trim());
+                var reveals = dataSplit[1].Split(';');
+                bool isPossible = true;
+
+
+                if(isPossible)
+                    sumOfAll += gameid;
+            }
+
+            return sumOfAll;
+        }
+    }
+}
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -4,13 +4,18 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var day1 = new Day1();
+        //var day1 = new Day1();
 
         //var day1Result = day1.SumAllCalibrationValues();
         //Console.WriteLine($"Day 1 - sum of all calibration values is:{day1Result}");
-        
-        var day1Part2Result = day1.SumAllCalibrationValuesPartTwo();
-        Console.WriteLine($"Day 1 - sum of all calibration values part two is:{day1Part2Result}");
+
+        //var day1Part2Result = day1.SumAllCalibrationValuesPartTwo();
+        //Console.WriteLine($"Day 1 - sum of all calibration values part two is:{day1Part2Result}");
+
+        var day2 = new Day2();
+
+        var day2Part1Result = day2.SumOfPossibleGameIds();
+        Console.WriteLine($"Day 2 - sum of all possible game ids is:{day2Part1Result}");
 
     }
 }
